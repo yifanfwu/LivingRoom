954ef93845564c8709da0942e104fe4b1d7f8463
Yifan Wu <yifanfwu@gmail.com>
2015-07-07 23:17:47 -0400

    connected to firebase

--- app/src/main/java/com/yifanfwu/livingroom/MainActivity.java
+++ app/src/main/java/com/yifanfwu/livingroom/MainActivity.java
@@ -1,9 +1,19 @@
 package com.yifanfwu.livingroom;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +21,58 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button taskButton = (Button) findViewById(R.id.task_button);
+        taskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TaskActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        Firebase.setAndroidContext(this);
+//        final Firebase ref = new Firebase("https://livingroom.firebaseio.com/");
+//
+//        final EditText taskName = (EditText) findViewById(R.id.task_name);
+//
+//        Button taskButton = (Button) findViewById(R.id.task_button);
+//        taskButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ref.child("tasks").push().setValue(taskName.getText().toString());
+//                taskName.setText("");
+//            }
+//        });
+//
+//        ref.child("tasks").addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                Toast.makeText(getApplicationContext(), "Child added", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//                Toast.makeText(getApplicationContext(), "Child removed", Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(FirebaseError firebaseError) {
+//
+//            }
+//        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/yifanfwu/livingroom/TaskActivity.java
@@ -0,0 +1,87 @@
+package com.yifanfwu.livingroom;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+public class TaskActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+        Firebase.setAndroidContext(this);
+        final Firebase ref = new Firebase("https://livingroom.firebaseio.com/");
+
+        final EditText taskName = (EditText) findViewById(R.id.task_edittext);
+
+        Button taskButton = (Button) findViewById(R.id.add_task_button);
+        taskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ref.child("tasks").push().setValue(taskName.getText().toString());
+                taskName.setText("");
+            }
+        });
+
+        ref.child("tasks").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Toast.makeText(getApplicationContext(), "Child added", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Toast.makeText(getApplicationContext(), "Child removed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_task, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

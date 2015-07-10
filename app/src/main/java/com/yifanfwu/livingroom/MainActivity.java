84e8e940b3cb909b31145a89651377bdefbaaee3
Yifan Wu <yifanfwu@gmail.com>
2015-07-09 23:41:03 -0400

    deleting tasks now works

--- app/src/main/java/com/yifanfwu/livingroom/MainActivity.java
+++ app/src/main/java/com/yifanfwu/livingroom/MainActivity.java
@@ -3,24 +3,29 @@ package com.yifanfwu.livingroom;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Button taskButton = (Button) findViewById(R.id.task_button);
         taskButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/yifanfwu/livingroom/TaskActivity.java
+++ app/src/main/java/com/yifanfwu/livingroom/TaskActivity.java
@@ -1,17 +1,20 @@
 package com.yifanfwu.livingroom;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
@@ -21,30 +24,35 @@ import com.firebase.client.Query;
 
 import java.util.ArrayList;
 
-public class TaskActivity extends Activity {
+public class TaskActivity extends AppCompatActivity {
 
     private static final String TAG = "TaskActivity";
+
+    final ArrayList<String> stringList = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         Firebase.setAndroidContext(this);
         final Firebase ref = new Firebase("https://livingroom.firebaseio.com/");
 
-        final EditText taskName = (EditText) findViewById(R.id.task_edittext);
-
-        Button taskButton = (Button) findViewById(R.id.add_task_button);
+        FloatingActionButton taskButton = (FloatingActionButton) findViewById(R.id.add_task_button);
+//        Button taskButton = (Button) findViewById(R.id.add_task_button);
         taskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ref.child("tasks").push().setValue(taskName.getText().toString());
-                taskName.setText("");
+//                ref.child("tasks").push().setValue(taskName.getText().toString());
+//                taskName.setText("");
+                showAddTaskDialog(ref);
             }
         });
-
-        final ArrayList<String> stringList = new ArrayList<>();
 
         final ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringList);
         ListView taskList = (ListView) findViewById(R.id.task_list);
@@ -66,7 +74,11 @@ public class TaskActivity extends Activity {
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
                 String task = (String) dataSnapshot.getValue();
-                stringList.remove(task);
+                try {
+                    stringList.remove(task);
+                } catch (Exception e) {
+                    Log.d(TAG, "File already deleted");
+                }
                 listAdapter.notifyDataSetChanged();
             }
 
@@ -85,8 +97,35 @@ public class TaskActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //need to delete from database
+                String value = stringList.get(position);
+                Query query = ref.child("tasks").orderByValue().equalTo(value);
+                query.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        ref.child("tasks").child(dataSnapshot.getKey()).removeValue();
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+
+                    }
+                });
                 stringList.remove(position);
-                listAdapter.notifyDataSetChanged();
             }
         });
     }
@@ -111,5 +150,33 @@ public class TaskActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void showAddTaskDialog(final Firebase ref) {
+
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(TaskActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.dialog_add_task, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(TaskActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.edit_text);
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ref.child("tasks").push().setValue(editText.getText().toString());
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 }
